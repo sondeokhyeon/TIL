aedd7eea1f07eb3636084582b65fe81c089ddfe7
손덕현 <sondeokhyeon@gmail.com>
2018-05-17 23:03:35 +0900

    COMMIT

---
+++ movie-app_npm/src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './App.css';
+import Movie from './Movie.js';
+
+class App extends Component {
+
+ state = {}
+
+ componentDidMount() {
+  this._getMovies();
+ }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map(movie => {
+      console.log(movie)
+      return <Movie 
+        title={movie.title_english} 
+        poster={movie.medium_cover_image} 
+        key={movie.id} 
+        genres={movie.genres}
+        synopsis={movie.synopsis}  
+      />
+    }) 
+    return movies
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(potato => potato.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.movies ? this._renderMovies() : 'Loading'}    
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ movie-app_npm/src/Movie.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types'; // npm i prop-types 으로 설치 가능
+import './Movie.css';
+
+function Movie({title, poster, genres, synopsis}) {
+    return (
+        <div className="Movie">
+            <div className="Movie__Columns">
+                <MoviePoster poster={poster} alt={title}/>
+            </div>
+            <div className="Movie__Columns">
+                <h1>{title}</h1>
+                <div className="Movie__Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+                <p className="Movie__Synopsis">
+                    {synopsis}
+                </p>
+
+            </div>
+        </div>
+    )
+}
+
+function MoviePoster ({poster, alt}) {
+    return (
+        <img src={poster} alt={alt} className="Movie__Poster"/>
+    )
+}
+
+function MovieGenre({genre}) {
+    return (
+        <span className="Movie__genre">{genre}</span>
+    )   
+}
+
+Movie.propTypes = {
+    title    : PropTypes.string.isRequired,
+    poster   : PropTypes.string.isRequired,
+    genres   : PropTypes.array.isRequired,
+    synopsis : PropTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+    poster   : PropTypes.string.isRequired
+}
+
+MovieGenre.propTypes = {
+    genre   : PropTypes.string.isRequired
+}
+
+
+
+
+
+export default Movie
