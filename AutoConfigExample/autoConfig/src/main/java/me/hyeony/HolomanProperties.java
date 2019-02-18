19414403f013f32f7acbf49f1579a142fecc813f
손덕현 <sondeokhyeon@gmail.com>
2019-02-18 23:14:38 +0900

    init

---
+++ AutoConfigExample/autoConfig/src/main/java/me/hyeony/HolomanProperties.java
@@ -0,0 +1,27 @@
+package me.hyeony;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("holoman")
+public class HolomanProperties {
+
+    private String name;
+    private int howLong;
+
+    public int getHowLong() {
+        return howLong;
+    }
+
+    public void setHowLong(int howLong) {
+        this.howLong = howLong;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ AutoConfigExample/autoConfigRunProject/src/main/java/main/Application.java
@@ -0,0 +1,16 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Application.class);
+        application.setWebApplicationType(WebApplicationType.NONE);
+        application.run(args);
+    }
+
+}
