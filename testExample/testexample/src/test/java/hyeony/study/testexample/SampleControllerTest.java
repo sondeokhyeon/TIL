e81fb9be22df59cdf139260a9bf67cce657b7cdc
손덕현 <sondeokhyeon@gmail.com>
2019-03-02 00:00:59 +0900

    Today I Learn

--- testExample/testexample/src/test/java/hyeony/study/testexample/SampleControllerTest.java
+++ testExample/testexample/src/test/java/hyeony/study/testexample/SampleControllerTest.java
@@ -5,10 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
@@ -28,18 +27,28 @@ public class SampleControllerTest {
     }
     */
 
+    //@Autowired
+    //TestRestTemplate testRestTemplate;
+
     @Autowired
-    TestRestTemplate testRestTemplate;
+    WebTestClient webTestClient;
 
     @MockBean
     SampleService mockSampleService;
 
     @Test
     public void hello() throws Exception {
-        when(mockSampleService.getName()).thenReturn("hyeony");
+        when(mockSampleService.getName())
+                .thenReturn("hyeony");
 
+        webTestClient.get().uri("/hello").exchange()
+                        .expectStatus().isOk()
+                        .expectBody(String.class).isEqualTo("Hello hyeony");
+
+        /*
         String result = testRestTemplate.getForObject("/hello", String.class);
         assertThat(result).isEqualTo("Hello hyeony");
+        */
     }
 
 }
