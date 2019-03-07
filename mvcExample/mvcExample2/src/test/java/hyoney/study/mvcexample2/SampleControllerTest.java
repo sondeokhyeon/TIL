00df9fb8fde14164c1e4bd93634565cb54486eba
손덕현 <sondeokhyeon@gmail.com>
2019-03-07 23:49:56 +0900

    Add : Today I Learn

--- mvcExample/mvcExample2/src/test/java/hyoney/study/mvcexample2/SampleControllerTest.java
+++ mvcExample/mvcExample2/src/test/java/hyoney/study/mvcexample2/SampleControllerTest.java
@@ -1,30 +1,31 @@
 package hyoney.study.mvcexample2;
 
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(SampleController.class)
+@WebMvcTest(SampleController2.class)
 public class SampleControllerTest {
 
     @Autowired
-    WebClient webClient;
-    //MockMvc mockMvc;
+    //WebClient webClient;
+            MockMvc mockMvc;
 
     @Test
     public void hello() throws Exception {
 
-        HtmlPage page = webClient.getPage("/hello1");
-        HtmlHeading1 h1 = page.getFirstByXPath("//h1");
-        assertThat(h1.getTextContent()).isEqualToIgnoringCase("hyeony1");
+    //    HtmlPage page = webClient.getPage("/hello1");
+    //    HtmlHeading1 h1 = page.getFirstByXPath("//h1");
+    //    assertThat(h1.getTextContent()).isEqualToIgnoringCase("hyeony1");
 
         /*
         mockMvc.perform(get("/hello1"))
@@ -34,6 +35,11 @@ public class SampleControllerTest {
                 .andExpect(model().attribute("name", is("hyeony")))
                 .andExpect(content().string(containsString("hyeony")));
         */
+
+        mockMvc.perform(get("/hello3"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._links.self").exists());
     }
 
 }
