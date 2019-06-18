e5aca891f926eed6d2038a579eee272f0c3d361b
손덕현 <sondeokhyeon@gmail.com>
2019-06-18 22:22:33 +0900

    Modify : Today I Learn

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import React from "react";
-import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import React, { Component } from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import Weather from "./weather";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <ActivityIndicator size="large" />
-      <Text>Hello ReactNative!@!!</Text>
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    isLoaded: false
+  };
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          isLoaded: true
+        });
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+  render() {
+    const { isLoaded } = this.state;
+    return (
+      <LinearGradient colors={["#005BEA", "#00C6FB"]} style={styles.container}>
+        <StatusBar hidden={true} />
+        {isLoaded ? (
+          <Weather />
+        ) : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the weather</Text>
+          </View>
+        )}
+      </LinearGradient>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  loading: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#FDF6AA",
+    justifyContent: "flex-end",
+    paddingLeft: 25
+  },
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 100
   }
 });
