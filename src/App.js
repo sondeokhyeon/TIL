1751f395dcc00636b3bae3d3169c79cfe99a214e
손덕현 <sondeokhyeon@gmail.com>
2019-07-02 16:16:55 +0900

    Today I Learn

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Header />
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>My Contacts</h1>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/contacts">contacts</Link>
+        </li>
+      </ul>
+    </nav>
+    <Route exact path="/" component={Welcome} />
+    <Route path="/contacts" component={contacts} />
+  </header>
+);
+
+const Welcome = () => <h1>Welcome to the contact App!</h1>;
+
+const contacts = ({ match }) => (
+  <div>
+    <ul>
+      <li>
+        <Link to="/contacts/hong">hong</Link>
+      </li>
+      <li>
+        <Link to="/contacts/kim">kim</Link>
+      </li>
+      <li>
+        <Link to="/contacts/park">park</Link>
+      </li>
+    </ul>
+    <Route
+      exact
+      path={`${match.path}`}
+      render={() => <h3>please select a contact</h3>}
+    />
+    <Route path={`${match.path}/:contactName`} component={Contact} />
+  </div>
+);
+
+const Contact = ({ match }) => `name is ${match.params.contactName}`;
 
 export default App;
