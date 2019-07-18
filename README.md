1e41b95db457f350e18c90dd8fb78db4d0f69b60
손덕현 <sondeokhyeon@gmail.com>
2019-07-18 22:33:53 +0900

    Today I Learn

---
+++ README.md
@@ -0,0 +1,1 @@
+webpacki basic Practice
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"]
+        }
+      }
+    ]
+  }
+};
