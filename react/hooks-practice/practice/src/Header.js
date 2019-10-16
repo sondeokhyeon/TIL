cbf92220fbac3406e27452000b488760bf9bb20d
손덕현 <sondeokhyeon@gmail.com>
2019-10-16 13:27:45 +0900

    Today I Learn

--- react/hooks-practice/practice/src/Header.js
+++ react/hooks-practice/practice/src/Header.js
@@ -3,12 +3,13 @@ import { Link, Switch } from "react-router-dom";
 import  './scss/Header.scss'
 
 const Header = () => (
-    <Switch>  
-        <div className="header">
-                <span><Link to="/">SetState</Link></span>
-                <span><Link to="input">SetInput</Link></span>
-        </div>
-        <hr/>
+    <Switch>
+        <>  
+        <header className="header">
+            <span><Link to="/">SetState</Link></span>
+            <span><Link to="input">SetInput</Link></span>
+        </header>
+        </>
     </Switch>
 )
 
--- react/hooks-practice/practice/src/use/Input.js
+++ react/hooks-practice/practice/src/use/Input.js
@@ -1,1 +1,67 @@
-export default () => "input"
+import React, {useState} from "react"
+
+const useInput = (initValue, validate) => {
+    const [value, setValue] = useState(initValue)
+    const onChange = (event) =>  {
+        const {
+            target : { value }
+        } = event;
+        let willUpdate = true;
+        if(typeof validate === "function") {
+            willUpdate = validate(value)
+        }
+        if(willUpdate) {
+            setValue(value);
+        }
+    };
+    return { value, onChange };
+}
+
+const Input = () => {
+    const maxLen = value => value.length <= 10 || value.includes("@");
+    const name = useInput("Mr.", maxLen);
+    return (
+        <div>
+        <div>
+            <h1>Use Input</h1>
+            <input placeholder="Name" {...name}/>
+            {/* <input placeholder="Name" value={name.value} onChange={name.onChange}/> */}
+        </div>
+        <hr/>
+        <h1>CODE</h1>
+        <pre>{`const useInput = (initValue, validate) => {
+const [value, setValue] = useState(initValue)
+const onChange = (event) =>  {
+        const {
+            target : { value }
+        } = event;
+        let willUpdate = true;
+        if(typeof validate === "function") {
+            willUpdate = validate(value)
+        }
+        if(willUpdate) {
+            setValue(value);
+        }
+    };
+    return { value, onChange };
+}
+
+const Input = () => {
+    const name = useInput("Mr.")
+    return (
+        <div>
+        <div>
+            <h1>Input</h1>
+            <input placeholder="Name" {...name}/>
+            {/* <input placeholder="Name" value={name.value} onChange={name.onChange}/> */}
+        </div>
+    )
+}
+`}
+</pre>
+        <span> useState 활용 </span>
+        </div>
+    )
+}
+export default Input
+
