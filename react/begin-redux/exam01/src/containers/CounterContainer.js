0600bc41a8c57a37eb257ff9f8f89c4626b07365
sondeokhyeon <sondeokhyeon@gmail.com>
2020-04-08 16:36:22 +0900

    redux study

---
+++ react/begin-redux/exam01/src/containers/CounterContainer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Counter from "../components/Counter";
+import { decrease, increase, setDiff } from "../modules/counter";
+import { useSelector, useDispatch } from "react-redux";
+
+const CounterContainer = () => {
+  const { number, diff } = useSelector((state) => ({
+    number: state.counter.number,
+    diff: state.counter.diff,
+  }));
+  const dispatch = useDispatch();
+
+  const onIncrease = () => dispatch(increase());
+  const onDecrease = () => dispatch(decrease());
+  const onSetDiff = (diff) => dispatch(setDiff(diff));
+
+  return (
+    <Counter
+      number={number}
+      diff={diff}
+      onDecrease={onDecrease}
+      onIncrease={onIncrease}
+      onSetDiff={onSetDiff}
+    />
+  );
+};
+
+export default CounterContainer;
