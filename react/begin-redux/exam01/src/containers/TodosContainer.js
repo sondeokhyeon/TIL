d4a925ce1d9de5b94b3fd0bb96df2aa3d66d2775
sondeokhyeon <sondeokhyeon@gmail.com>
2020-04-13 16:51:19 +0900

    TIL redux stduy...

--- react/begin-redux/exam01/src/containers/TodosContainer.js
+++ react/begin-redux/exam01/src/containers/TodosContainer.js
@@ -1,16 +1,20 @@
 import React, { useCallback } from "react";
 import Todos from "../components/Todos";
-import { useSelector, useDispatch } from "react-redux";
+import { connect } from "react-redux";
 import { addTodo, toggleTodo } from "../modules/todos";
 
-function TodosContainer() {
-  const todos = useSelector((state) => state.todos);
-  const dispatch = useDispatch();
-
-  const onCreate = useCallback((text) => dispatch(addTodo(text)), [dispatch]);
-  const onToggle = useCallback((id) => dispatch(toggleTodo(id)), [dispatch]);
+function TodosContainer({ todos, addTodo, toggleTodo }) {
+  const onCreate = useCallback((text) => addTodo(text), [addTodo]);
+  const onToggle = useCallback((id) => toggleTodo(id), [toggleTodo]);
 
   return <Todos todos={todos} onCreate={onCreate} onToggle={onToggle} />;
 }
 
-export default TodosContainer;
+const mapStateToProps = (state) => ({ todos: state.todos });
+
+const mapDispatchToProps = {
+  addTodo,
+  toggleTodo,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodosContainer);
