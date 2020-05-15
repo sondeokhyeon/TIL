edf2be6274c94ec9411d5aec8336529f91ae5b7d
sondeokhyeon <sondeokhyeon@gmail.com>
2020-05-15 17:07:57 +0900

    TIL - springboot - oauth

---
+++ SpringBoot/OAuthTest/src/main/java/com/my/test/OAuthTestApplication.java
@@ -0,0 +1,13 @@
+package com.my.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthTestApplication.class, args);
+	}
+
+}
