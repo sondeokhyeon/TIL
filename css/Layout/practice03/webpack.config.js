b7d79e1534c0fb2a033dcd76d991d7688b6cc4c3
sondeokhyeon <sondeokhyeon@gmail.com>
2020-06-23 14:43:34 +0900

    TIL - css - layout - init

---
+++ css/Layout/practice03/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "style", "scss", "style.scss"),
+  output: {
+    path: path.resolve(__dirname, "style", "css"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [miniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new miniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
+};
