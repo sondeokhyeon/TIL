9d3c8dfca811dd9fafa12af4668fcb788a0ff064
sondeokhyeon <sondeokhyeon@gmail.com>
2020-12-16 13:39:36 +0900

    TIL - vue - crud

---
+++ vue/basic/study02/src/components/Read.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+    <table>
+        <tr>
+            <td>글쓴이</td>
+            <td>제목</td>
+            <td>내용</td>
+        </tr>
+        <tr v-for="(value, index) in data" :key="index" @click="detail(index)">
+            <td>{{value.writer}}<td>
+            <td>{{value.title}}</td>
+            <td>{{value.content}}</td>
+        </tr>
+    </table>
+    <button @click="writer">글쓰기</button>
+    </div>
+</template>
+
+
+<script>
+import data from '@/data'
+    export default {
+        name:'Read',
+        data() {
+            return {
+                data : data
+            }
+        },
+        methods : {
+            writer() {
+                this.$router.push({
+                    path : 'create'
+                })
+            }, 
+            detail(idx) {
+                this.$router.push({
+                    name:'Detail',
+                    params: {
+                        contentId:idx
+                    }
+                })
+            }
+        }
+    }
+</script>
---
+++ vue/basic/study02/src/data/index.js
@@ -0,0 +1,18 @@
+
+export default [
+    {
+        writer : 'A',
+        title : 'a',
+        content : 'Aa'
+    },
+    {
+        writer : 'B',
+        title : 'b',
+        content : 'Bb'
+    },
+    {
+        writer : 'C',
+        title : 'c',
+        content : 'Cc'
+    },
+]
