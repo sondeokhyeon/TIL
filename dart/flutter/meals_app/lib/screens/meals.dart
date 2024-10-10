f0abed0a8bce02d100f6541a6c766f84e4fe7fb4
sondeokhyeon <sondeokhyeon@gmail.com>
2024-10-10 13:01:36 +0900

    TIL - flutter - screen add

---
+++ dart/flutter/meals_app/lib/screens/meals.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({
+    super.key,
+    required this.title,
+    required this.meals,
+  });
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals'),
+      ),
+      body: Center(
+        child: Text('The meals will be listed here.'),
+      ),
+    );
+  }
+}
