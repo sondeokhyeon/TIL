2c26823576cc2752bd8cf21f1b40f12d2bdb8961
sondeokhyeon <sondeokhyeon@gmail.com>
2024-10-14 12:54:42 +0900

    TIL - flutter - material navigator apply & data filterd

--- dart/flutter/meals_app/lib/screens/categires.dart
+++ dart/flutter/meals_app/lib/screens/categires.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummey_data.dart';
 import 'package:meals_app/wigets/category_grid_item.dart';
+import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/models/category.dart';
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filterdMeals = dummyMeals
+        .where((meals) => meals.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(title: 'title', meals: filterdMeals),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +35,12 @@ class CategoryScreen extends StatelessWidget {
         ),
         children: [
           for (final category in availableCategories)
-            CategoryGridItem(category: category)
+            CategoryGridItem(
+              category: category,
+              selectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
         ],
       ),
     );
