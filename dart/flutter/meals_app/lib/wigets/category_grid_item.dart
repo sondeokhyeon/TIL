0453f04ac2faf407e4d994430a6c8ceb8dc900ca
sondeokhyeon <sondeokhyeon@gmail.com>
2024-10-14 12:54:19 +0900

    TIL - flutter - material navigator apply

--- dart/flutter/meals_app/lib/wigets/category_grid_item.dart
+++ dart/flutter/meals_app/lib/wigets/category_grid_item.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
-  const CategoryGridItem({super.key, required this.category});
+  const CategoryGridItem(
+      {super.key, required this.category, required this.selectCategory});
 
   final Category category;
+  final void Function() selectCategory;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {
-        //print('hello');
-      },
+      onTap: selectCategory,
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(16),
       child: Container(
