092b37ea46147abb694c020c83699e9830a8c172
sondeokhyeon <sondeokhyeon@gmail.com>
2024-10-16 12:53:58 +0900

    TIL - flutter - detail Info Screen add

--- dart/flutter/meals_app/lib/screens/meals.dart
+++ dart/flutter/meals_app/lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/wigets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
@@ -39,9 +40,8 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (ctx, index) => Text(
-          meals[index].title,
-          style: const TextStyle(color: Colors.white),
+        itemBuilder: (ctx, index) => MealItem(
+          meal: meals[index],
         ),
       );
     }
