8430bb9f5119e5bfc89e69c9ed6ab2cbfc0ae8df
sondeokhyeon <sondeokhyeon@gmail.com>
2024-10-24 23:10:15 +0900

    TIL - flutter - unuse properties delete

--- dart/flutter/meals_app/lib/main.dart
+++ dart/flutter/meals_app/lib/main.dart
@@ -4,7 +4,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:meals_app/screens/tabs.dart';
 
 final theme = ThemeData(
-  useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
     brightness: Brightness.dark,
     seedColor: const Color.fromARGB(255, 131, 57, 0),
