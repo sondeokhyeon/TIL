5e8e77740b3c715eb6926cbde156a7fc15aa8ca0
sondeokhyeon <sondeokhyeon@gmail.com>
2024-10-24 23:11:39 +0900

    TIL - flutter - filter screen add

---
+++ dart/flutter/meals_app/lib/screens/filters.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+//import 'package:meals_app/screens/tabs.dart';
+//import 'package:meals_app/wigets/main_drawer.dart';
+
+class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({super.key});
+
+  @override
+  State<FiltersScreen> createState() {
+    return _FiltersScreenState();
+  }
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _glutenFreeFilterSet = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Filters'),
+      ),
+      //drawer: MainDrawer(
+      //  onSelectScreen: (identifire) {
+      //  Navigator.of(context).pop();
+      //  if (identifire == 'meals') {
+      //    Navigator.of(context).pushReplacement(
+      //      MaterialPageRoute(
+      //        builder: (x) => const TabScreen(),
+      //      ),
+      //    );
+      //  }
+      //},
+      //),
+      body: Column(
+        children: [
+          SwitchListTile(
+            value: _glutenFreeFilterSet,
+            onChanged: (isChecked) {
+              setState(() {
+                _glutenFreeFilterSet = isChecked;
+              });
+            },
+            title: Text(
+              'Gluten-free',
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onSurface),
+            ),
+            subtitle: Text(
+              'Only includes Gluten-free meals',
+              style: Theme.of(context)
+                  .textTheme
+                  .labelMedium!
+                  .copyWith(color: Theme.of(context).colorScheme.onSurface),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(
+              left: 34,
+              right: 22,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
