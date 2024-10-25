a25dbb37809177b13c077a59a5c9a290d3198543
sondeokhyeon <sondeokhyeon@gmail.com>
2024-10-25 13:07:43 +0900

    TIL - flutter - filter PopScope apply

--- dart/flutter/meals_app/lib/screens/tabs.dart
+++ dart/flutter/meals_app/lib/screens/tabs.dart
@@ -46,14 +46,15 @@ class _TabScreenState extends State<TabScreen> {
     });
   }
 
-  void _setScreen(String identifier) {
+  void _setScreen(String identifier) async {
     Navigator.of(context).pop();
     if (identifier == 'filters') {
-      Navigator.of(context).push(
+      final result = await Navigator.of(context).push<Map<Filters, bool>>(
         MaterialPageRoute(
           builder: (x) => const FiltersScreen(),
         ),
       );
+      print(result);
     }
   }
 
