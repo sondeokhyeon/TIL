0b4ee5c6835741303893c42ea571ee6cf50fccab
sondeokhyeon <sondeokhyeon@gmail.com>
2024-11-04 12:25:40 +0900

    TIL - flutter - filter provider add

---
+++ dart/flutter/meals_app/lib/providers/filters_provders.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum Filters {
+  glutenFree,
+  lactoseFree,
+  vegetarian,
+  vegan,
+}
+
+class FiltersNotifier extends StateNotifier<Map<Filters, bool>> {
+  FiltersNotifier()
+      : super({
+          Filters.glutenFree: false,
+          Filters.lactoseFree: false,
+          Filters.vegetarian: false,
+          Filters.vegan: false,
+        });
+
+  void setFilter(Filters filter, bool isActive) {
+    //state[filter] = isActive;
+    state = {
+      ...state,
+      filter: isActive,
+    };
+  }
+}
+
+final filtersProviders =
+    StateNotifierProvider<FiltersNotifier, Map<Filters, bool>>(
+  (ref) => FiltersNotifier(),
+);
