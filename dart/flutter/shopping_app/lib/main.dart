9b047e9984cf65bc50ba647077f09043b576ff83
sondeokhyeon <sondeokhyeon@gmail.com>
2024-11-14 12:30:02 +0900

    TIL - flutter - ui practise

--- dart/flutter/shopping_app/lib/main.dart
+++ dart/flutter/shopping_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_app/widgets/main-scroll.dart';
+import 'package:shopping_app/widgets/groceries_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         ),
         scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
       ),
-      home: const MainScroll(),
+      home: const GroceriesList(),
     );
   }
 }
