2c1a85f747e27bfb037fb9954b5d96aeb5c6c51e
sondeokhyeon <sondeokhyeon@gmail.com>
2024-11-15 13:04:19 +0900

    TIL - flutter - main widget navigate add

--- dart/flutter/shopping_app/lib/widgets/groceries_list.dart
+++ dart/flutter/shopping_app/lib/widgets/groceries_list.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/data/dummy_items.dart';
+import 'package:shopping_app/widgets/new_item.dart';
 
-class GroceriesList extends StatelessWidget {
+class GroceriesList extends StatefulWidget {
   const GroceriesList({super.key});
+
+  @override
+  State<GroceriesList> createState() => _GroceriesListState();
+}
+
+class _GroceriesListState extends State<GroceriesList> {
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+  }
 
   @override
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          )
+        ],
       ),
       body: ListView.builder(
         padding: const EdgeInsets.symmetric(horizontal: 20),
