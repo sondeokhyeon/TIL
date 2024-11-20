f82d35ca0f5d0fabca1c6a06b2167576a819ebc1
sondeokhyeon <sondeokhyeon@gmail.com>
2024-11-20 12:56:14 +0900

    TIL - flutter - validation and submit apply

--- dart/flutter/shopping_app/lib/widgets/new_item.dart
+++ dart/flutter/shopping_app/lib/widgets/new_item.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:shopping_app/data/categories.dart';
+import 'package:shopping_app/models/category.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -11,9 +11,17 @@ class NewItem extends StatefulWidget {
 
 class _NewItemState extends State<NewItem> {
   final _formKey = GlobalKey<FormState>();
+  var _enteredName = '';
+  var _enteredQuantity = 0;
+  var _selectedCategory = categories[Categories.vegetables];
 
   void _saveItem() {
-    _formKey.currentState!.validate();
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      print(_enteredName);
+      print(_enteredQuantity);
+      print(_selectedCategory!.title);
+    }
   }
 
   @override
@@ -42,6 +50,11 @@ class _NewItemState extends State<NewItem> {
                   }
                   return null;
                 },
+                onSaved: (value) {
+                  setState(() {
+                    _enteredName = value!;
+                  });
+                },
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -61,6 +74,9 @@ class _NewItemState extends State<NewItem> {
                           return 'Must be valid positive number';
                         }
                         return null;
+                      },
+                      onSaved: (value) {
+                        _enteredQuantity = int.parse(value!);
                       },
                     ),
                   ),
@@ -84,7 +100,11 @@ class _NewItemState extends State<NewItem> {
                             ),
                           )
                       ],
-                      onChanged: (value) {},
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedCategory = value!;
+                        });
+                      },
                     ),
                   ),
                 ],
