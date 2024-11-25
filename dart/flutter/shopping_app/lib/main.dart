2ab941e4ff64d9d37aba38b6d6d17b9b4fc87708
sondeokhyeon <sondeokhyeon@gmail.com>
2024-11-25 12:16:00 +0900

    TIL - flutter - http overrides apply

--- dart/flutter/shopping_app/lib/main.dart
+++ dart/flutter/shopping_app/lib/main.dart
@@ -1,8 +1,20 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_app/widgets/groceries_list.dart';
 
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
 }
 
 class MyApp extends StatelessWidget {
