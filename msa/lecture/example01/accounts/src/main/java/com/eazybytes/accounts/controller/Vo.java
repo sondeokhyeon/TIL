10ddff93ef0ac7df61f00275aa1cea887c517d0a
sondeokhyeon <sondeokhyeon@gmail.com>
2024-12-06 12:28:39 +0900

    TIL - MSA - SpringBoot Lombok Test

---
+++ msa/lecture/example01/accounts/src/main/java/com/eazybytes/accounts/controller/Vo.java
@@ -0,0 +1,13 @@
+
+package com.eazybytes.accounts.controller;
+
+import lombok.Data;
+
+@Data
+public class Vo {
+    private String id;
+    private int age;
+    private String name;
+    private String address;
+
+}
