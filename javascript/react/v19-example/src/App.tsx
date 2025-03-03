ae9f328144734292bfbcc3483548c57448884bc2
sondeokhyeon <sondeokhyeon@gmail.com>
2025-03-04 00:50:30 +0900

    TIL - react - v19 version hooks remove unuse code

--- javascript/react/v19-example/src/App.tsx
+++ javascript/react/v19-example/src/App.tsx
@@ -1,36 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Optimistic } from './hooks/Optimistic'
 import { Transition } from './hooks/Transition'
 
 function App() {
-    const [count, setCount] = useState(0)
 
     return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
+        <div style={{ gap: '30px', display: 'flex', flexDirection: 'column' }}>
             <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
             <Transition />
-        </>
+            <Optimistic />
+        </div>
     )
 }
 
