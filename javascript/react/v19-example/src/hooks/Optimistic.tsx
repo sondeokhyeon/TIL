bfbfae138ba636c0a0245bd83cfbfaac519f8e2b
sondeokhyeon <sondeokhyeon@gmail.com>
2025-03-04 00:50:43 +0900

    TIL - react - v19 version hooks add useOptimistic

---
+++ javascript/react/v19-example/src/hooks/Optimistic.tsx
@@ -0,0 +1,45 @@
+import { useState, useOptimistic, startTransition } from "react"
+
+
+export const Optimistic = () => {
+    const [isActive, setIsActive] = useState(false)
+
+    const [optimisticIsOn, addOptimisticIsOn] = useOptimistic(
+        isActive,
+        (currentState, newState) => !currentState
+        //newState A
+    );
+
+    const toggleWithDelay = () => {
+        // useOptimistic은 React Transition이나 액션 내에서만 업데이트를 해야 한다.
+        startTransition(async () => {
+
+            // 낙관적 UI 업데이트
+            addOptimisticIsOn(!isActive);
+
+            try {
+                // 서버 요청 시뮬레이션 (1초 지연)
+                await new Promise(resolve => setTimeout(resolve, 1000));
+                throw new Error('error')
+                // 실제 상태 업데이트
+                setIsActive(!isActive);
+            } catch (error) {
+                console.error("토글 작업 실패:", error);
+                // 오류 발생 시 낙관적 상태는 자동으로 원래 상태로 되돌아감
+            }
+
+        })
+    };
+
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <div style={{
+                width: '30px', height: '30px', border: '1px solid black',
+                ...(optimisticIsOn && { background: 'black' })
+            }}
+                onClick={() => {
+                    toggleWithDelay()
+                }} ></div>
+        </div >
+    )
+}
